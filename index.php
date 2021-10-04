15b73708ea3cd116385c82dd9cd194b7cdfc2021
Henry Maldonado <infonox67@gmail.com>
2021-10-03 21:25:29 -0400

    changed home.html

--- index.php
+++ index.php
@@ -1,4 +1,2 @@
 <?php require_once(dirname("ExerciseApp").'/staticpages/home.html')?>
-<?php include(dirname("ExerciseApp").'/staticpages/profile.html')?>
-<?php include(dirname("ExerciseApp").'/staticpages/contact.html')?>
-<?php include(dirname("ExerciseApp").'/staticpages/about.html')?>
+
